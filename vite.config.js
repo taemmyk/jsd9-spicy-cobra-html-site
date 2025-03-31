f4d962321473e0ccaeac1381d0341fcdc6aec6cc
tk <t.khamiam@gmail.com>
2025-03-31 22:46:12 +0700

    fixed tailwind vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about-spicy-cobra.html"),
+        game_detail: resolve(__dirname, "game_detail.html"),
+        game_categories: resolve(__dirname, "game_categories.html"),
+        order: resolve(__dirname, "order.html"),
+        checkout: resolve(__dirname, "checkout.html"),
+        devlogs: resolve(__dirname, "devlogs.html"),
+      },
+    },
+  },
+});
